8b0b97c982fcab5c8b1afe422bd1d834938cfa0d
huangjs888 <1170384651@qq.com>
2021-12-30 11:55:34 +0800

    生成umd文件

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-param-reassign */
 /*
  * @Author: Huangjs
  * @Date: 2021-10-10 15:07:28
  * @LastEditors: Huangjs
- * @LastEditTime: 2021-12-29 16:15:18
+ * @LastEditTime: 2021-12-30 11:34:17
  * @Description: ******
  */
 
-import timer from './timer';
-import dragOrder from './drag-order';
+import timer from './src/timer';
+import dragOrder from './src/drag-order';
 
 export { timer };
 export default dragOrder;
